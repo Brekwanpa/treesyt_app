49dd1a132999d8261692b084f83837b265dc5d69
Brekwanpa <qandynana001@gmail.com>
2026-06-22 16:36:55 +0000

    Update Send PREs UI

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2756554711" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2014968281" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
